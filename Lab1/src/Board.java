cffb61baef9c2657f9dea390e2bab8deb64e6f1f
Rafael Reis <dasmen12@gmail.com>
2021-10-25 03:28:28 +0100

    Need to finish children()

--- Lab1/src/Board.java
+++ Lab1/src/Board.java
@@ -1,4 +1,4 @@
-import java.util.List;
+import java.util.*;
 
 class Board implements Ilayout, Cloneable {
     private static final int dim=3;
@@ -19,20 +19,82 @@ class Board implements Ilayout, Cloneable {
     }
 
     public String toString() {
-        // To be completed
-        return null;
+        String result = "";
+        for(int i = 0; i < dim; i++) {
+            for(int j = 0; j < dim; j++) {
+                if(this.board[i][j] == 0)
+                    result += " ";
+                else result += this.board[i][j];
+            }
+            result += "\n";
+        }
+        return result;
+    }
+
+    private int[] modifierLocation(int[][] b) {
+        int[] result = new int[2];
+        for(int i = 0; i < dim; i++) {
+            for(int j = 0; j < dim; j++) {
+                if(this.board[i][j] == 0) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result; 
+    }
+
+    private int[][] moveNumbers(int[] coords, int modX, int modY) {
+        int[][] boardModded = new int[dim][dim];
+        int counter = this.board[modX][modY];
+
+        for(int i = 0; i < dim; i++) {
+            for(int j = 0; j < dim; j++) {
+                if(i == modX && j == modY)
+                    boardModded[i][j] = 0;
+                else if(i == coords[0] && j == coords[1])
+                    boardModded[i][j] = counter;
+                else boardModded[i][j] = this.board[i][j];
+            }
+        }
+        return boardModded;
     }
 
     @Override
     public List<Ilayout> children() {
-        // TODO Auto-generated method stub
-        return null;
+        List<Ilayout> result = new ArrayList<>();
+        int[] coords = new int[2];
+        Board newBoard;
+        coords = modifierLocation(this.board);
+        if(coords[1] - 1 >= 0)
+            newBoard.board = moveNumbers(coords, coords[0], coords[1]-1); result.add(newBoard);
+        if(coords[1] + 1 <= 3)
+            moveNumbers(coords, coords[0], coords[1]+1);
+        if(coords[0] - 1 >= 0)
+            moveNumbers(coords, coords[0]-1, coords[1]);
+        if(coords[1] + 1 <= 3)
+            moveNumbers(coords, coords[0], coords[1]+1);
+
+        return result;
     }
 
     @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Board b = (Board) o;
+            for(int i = 0; i < dim; i++) {
+                for(int j = 0; j < dim; j++) {
+                    if(b.board[i][j] != this.board[i][j])
+                        return false;
+                }
+            }
+            return true;
+        }
+
+    @Override
     public boolean isGoal(Ilayout l) {
-        // TODO Auto-generated method stub
-        return false;
+        return this.equals(l);
     }
 
     @Override
