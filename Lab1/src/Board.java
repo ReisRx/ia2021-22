c06fb0f45cb6c1c545dfd56df516f70bc424f785
Rafael Reis <dasmen12@gmail.com>
2021-10-25 03:18:55 +0100

    Falta o children

--- Lab1/src/Board.java
+++ Lab1/src/Board.java
@@ -31,10 +31,51 @@ class Board implements Ilayout, Cloneable {
         return result;
     }
 
+    private int[] modifierLocation(int[][] b) {
+        int[] result = new int[2];
+        for(int i = 0; i < dim; i++) {
+            for(int j = 0; j < dim; j++) {
+                if(this.board[i][j] == 0) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result; 
+    }
+
+    private int[][] moveNumbers(int[] coords, int modX, int modY) {
+        int[][] boardModded = new int[dim][dim];
+        int counter = this.board[modX][modY];
+
+        for(int i = 0; i < dim; i++) {
+            for(int j = 0; j < dim; j++) {
+                if(i == modX && j == modY)
+                    boardModded[i][j] = 0;
+                else if(i == coords[0] && j == coords[1])
+                    boardModded[i][j] = counter;
+                else boardModded[i][j] = this.board[i][j];
+            }
+        }
+        return boardModded;
+    }
+
     @Override
     public List<Ilayout> children() {
-        List<Ilayout> result = new ArrayList<Ilayout>();
-        
+        List<Ilayout> result = new ArrayList<>();
+        int[] coords = new int[2];
+        Board newBoard;
+        coords = modifierLocation(this.board);
+        if(coords[1] - 1 >= 0)
+            newBoard.board = moveNumbers(coords, coords[0], coords[1]-1); result.add(newBoard);
+        if(coords[1] + 1 <= 3)
+            moveNumbers(coords, coords[0], coords[1]+1);
+        if(coords[0] - 1 >= 0)
+            moveNumbers(coords, coords[0]-1, coords[1]);
+        if(coords[1] + 1 <= 3)
+            moveNumbers(coords, coords[0], coords[1]+1);
+
+        return result;
     }
 
     @Override
